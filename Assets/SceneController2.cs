6eac9df8db36cb036af4967e0868a3fd7c9e75f3
NicolasCCruz <nicolaschristophercruz@gmail.com>
2024-03-25 23:57:14 -0500

    Map2 additions

--- Assets/SceneController2.cs
+++ Assets/SceneController2.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;  // Necessary for changing scenes
 public class SceneController2 : MonoBehaviour
 {
     public GameObject jeanGameObject; // Assign your "Jean 1_0" GameObject in the inspector
+    public string targetSceneName; // Assign the target scene name in the Unity Editor
 
     // Update is called once per frame
     void Update()
@@ -13,9 +14,8 @@ public class SceneController2 : MonoBehaviour
         // Check if Jean's y position is at or above 4.74
         if(jeanGameObject.transform.position.y >= 4.74f)
         {
-            // Change to the scene named "map2"
-            SceneManager.LoadScene("map2");
+            // Change to the scene assigned in the targetSceneName variable
+            SceneManager.LoadScene(targetSceneName);
         }
     }
 }
-
